e8864001d6fe28d8ffc73671196c29d9e44b9356
Muhammad Taimour <muhammad.taimour95@gmail.com>
2019-10-26 11:05:28 +0500

    Complete Application

---
+++ resources/views/inc/messages.blade.php
@@ -0,0 +1,17 @@
+@if(count($errors) > 0)
+  @foreach($errors->all() as $error)
+  <div class="alert alert-danger">
+    {{$error}}
+  </div>
+  @endforeach
+@endif
+
+
+@if(session('success') > 0)
+ 
+  <div class="alert alert-success">
+    {{session('success')}}
+  </div>
+ 
+@endif
+
